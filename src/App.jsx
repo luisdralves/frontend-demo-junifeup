3ed93c4ae7891cf6117980bb4ec2f31f2223f4ef
luisdralves <luisalves@untile.pt>
2022-10-28 09:31:12 +0100

    readd navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './pages/home';
 import React from 'react';
 import './styles.css';
+import { Navbar } from './components/navbar';
 
 const App = () => (
   <BrowserRouter>
+    <Navbar />
+
     <div>
       <Route exact path="/" component={Home} />
     </div>
---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,32 @@
+import './styles.css';
+import { Link, useLocation } from 'react-router-dom';
+import React from 'react';
+
+const routes = [
+  {
+    href: '/',
+    label: 'Home',
+  },
+];
+
+export const Navbar = () => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+  return (
+    <header>
+      <span>Fake Twitter™️</span>
+      <ul className="header-links">
+        {routes.map(({ href, label }) => (
+          <li key={href}>
+            <Link
+              className={currentPath === href ? 'header-link-active' : ''}
+              to={href}
+            >
+              {label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </header>
+  );
+};
